883d144152b680d57cb4e72ab0d157858afd2e73
IonCatana <catana.ion17@yahoo.it>
2022-04-23 15:44:04 +0200

    fix view table trains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,49 @@
 @section('content')
     <div>
         @foreach ($trains as $item)
-            <div class="info">
-                <p>{{ $item->company }}</p>
-                <p>{{ $item->departure_station }}</p>
-                <p>{{ $item->arrival_station }}</p>
-                <p>{{ $item->departure_time }}</p>
-                <p>{{ $item->arrival_time }}</p>
-                <p>{{ $item->code }}</p>
-                <p>{{ $item->carriages }}</p>
-                <p>{{ $item->is_on_time }}</p>
-                <p>{{ $item->is_deleted }}</p>
+            <div class="contain">
+                <table class="info" style="width: 100%">
+                    <tr>
+                        <th>Company</th>
+                        <th>Departure Station</th>
+                        <th>Arrival Station</th>
+                        <th>Departure Time</th>
+                        <th>Arrival Time</th>
+                        <th>Code</th>
+                        <th>Carriages</th>
+                        <th>Is on Time</th>
+                        <th>Is Deleted</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="company">{{ $item->company }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->departure_station }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->arrival_station }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->departure_time }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->arrival_time }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->code }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->carriages }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->is_on_time }}</p>
+                        </td>
+                        <td>
+                            <p>{{ $item->is_deleted }}</p>
+                        </td>
+                    </tr>
+                </table>
             </div>
         @endforeach
     </div>
